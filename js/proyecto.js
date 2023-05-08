b6b90bfd3a40ddabf0e9fe7a899bbac0e592a8f2
MarisaSeg <mesthor683@gmail.com>
2023-05-07 22:21:08 -0300

    Página en HTML y CSS

---
+++ js/proyecto.js
@@ -0,0 +1,8 @@
+const hamb = document.querySelector('.hamb');//boton
+
+const enlaces = document.querySelector('.enlaces');//ul
+
+hamb.addEventListener('click', () => {
+    console.log(hamb);
+    // enlaces.classList.toggle('activado');
+});
